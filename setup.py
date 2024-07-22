cfe98dd4285ac8e5d187a326bad08573ca212c8c
Joe Wilson <96096412+PoliTwit1984@users.noreply.github.com>
2024-07-22 16:50:44 -0500

    Update URL to GitHub repository and add MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="LeonardoAIGenPy",
-    version="0.1.3",
+    version="0.1.4",
     author="Joe Wilson",
     author_email="joe.wilson@live.com",
     description="A Python package for interacting with Leonardo AI for image generation and upscaling.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/LeonardoAIGenPy",
+    url="https://github.com/Politwit1984/LeonardoAIGenPy",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
